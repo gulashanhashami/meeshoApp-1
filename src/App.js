69b23708f59b6b3688acba2720a1696e80831791
dilshad09 <dilshad_fw11_177@masai.school>
2022-02-25 18:33:42 +0530

    progress in routing all pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 
 import React from "react";
 import { Login } from "./components/authentication/login/LogIn";
-
+import {Product} from "./components/productdetails/productdetails"
 
 
  function App() {
   return <div>
-    
+    <Product/>
      <Login/>
     
   </div>
