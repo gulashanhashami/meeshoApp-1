93a8c24a39101024c8023269bbf394ed216d1354
dilshad09 <dilshad_fw11_177@masai.school>
2022-02-26 10:33:32 +0530

    some files deleted

--- src/App.js
+++ src/App.js
@@ -2,15 +2,15 @@
 import React from "react";
 import { Login } from "./components/authentication/login/LogIn";
 import {Product} from "./components/productdetails/productdetails"
-
-import { Home } from "./components/home/home";
+import {Home} from './components/home/Home'
 
  function App() {
   return <div>
     <Product/>
      <Login/>
     
-   
+   {/* <Home/> */}
+  
      
   </div>
    
