cfcca039c738cf561ccb263f908c39965816eb6d
dilshad09 <dilshad_fw11_177@masai.school>
2022-02-26 10:54:01 +0530

    home modified

--- src/App.js
+++ src/App.js
@@ -1,19 +1,41 @@
 
-import React from "react";
-import { Login } from "./components/authentication/login/LogIn";
-import {Product} from "./components/productdetails/productdetails"
-import {Home} from './components/home/Home'
+// import React from "react";
+// import { Login } from "./components/authentication/login/LogIn";
+// import {Product} from "./components/productdetails/productdetails"
+// import {Home} from './components/home/Home'
 
- function App() {
-  return <div>
-    <Product/>
-     <Login/>
+//  function App() {
+//   return <div>
+//     {/* <Product/>
+//      <Login/> */}
     
-   {/* <Home/> */}
+//    <Home/>
   
      
-  </div>
+//   </div>
    
   
+// }
+// export default App
+
+
+import './App.css';
+import { Home } from './components/home/Home';
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route
+  } from "react-router-dom";
+
+
+function App() {
+  return (
+    <div >
+      <Routes>
+            <Route path='/' element={<Home/>} />
+      </Routes>
+    </div>
+  );
 }
-export default App
+
+export default App;
